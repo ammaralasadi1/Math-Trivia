5c3fd796456c3d88907b4e8efae07c477e34adc7
Ammar Alasadi <alasadi.ammar@gmail.com>
2017-04-03 17:24:53 -0400

    Before scoring

--- cards.js
+++ cards.js
@@ -1,5 +1,8 @@
+
+
+
 var banner = $(".banner"); 
-var qfiled = $(".question");
+var qfield = $(".question");
 var input  = $("#result")
 var submit = $("#submit");
 var next = $("#next");
@@ -7,154 +10,56 @@ var start = $("#start");
 var right = ("Correct!");
 var wrong = ("Try again!")
 var show = $("#show");
+var start = $("#start");
+var score = [];
 
-var questions = {
-	a: "8 + 2 = ?", 
-	b: "5 + 3 = ?", 
-	c: "6 - 6 + 6 / 6 = ?",
-	d: "I know a three positive numbers that will results the same when multiplied together or added together",
-	f: "A merchant can place 8 large boxes or 10 small boxes into a carton for shipping. In one shipment, he sent a total of 96 boxes. If there are more large boxes than small boxes, how many cartons did he ship?",
-	g: "How many times can you take away 2 from 100?",
-	h: "There are 2 mothers, 2 daughters, 1 grandma and 1 grandaughter. How many people are there?"
+  
+
+var q_and_a = {
+
+	a: {question: "8 + 2 = ?", answer: 10, hint: "10"}  
+	,b: {question: "5 + 3 = ?", answer: 8, hint: "8"}  
+	,c: {question: "6 - 6 + 6 / 6 = ?", answer: 1, hint: "1"}
+	,d: {question: "Give me 3 positive numbers that will result the same when multiplied together or added together", answer: 123, hint: "1 + 2 + 3 = 6 and 1 * 2 * 3 = 6"}
+	,e: {question: "A merchant can place 8 large boxes or 10 small boxes into a carton for shipping. In one shipment, he sent a total of 96 boxes. If there are more large boxes than small boxes, how many cartons did he ship?", answer: 11, hint:"7 cartons for larges boxes per carton, where 7 x 8 = 56 boxes. 4 for small boxes, where 4 x 10 = 40 boxes. 96 total boxes and 11 cartons"}
+	,f: {question:"How many times can you take away 2 from 100?", answer : 1, hint: "Once! Because after that it's not 100 anymore"}
+	,g: {question:"There are 2 mothers, 2 daughters, 1 grandma and 1 grandaughter. How many people are there?", answer: 3, hint:"3 people. one mother is the mother of another daughter. (2 mothers) another mother's child and her child. (2 daughters). 1grandma the oldest one there. 1 grandaughter the smallest."}
 };
 
 
-//created questions 
+
+$(".questionButton").on('click', function() {
+
+	var id = this.id;
 
 
- $("#1").on("click", function(){
- 		
-       	qfiled.html(questions.a);
-        submit.on("click", function(){
-		var userInput = parseInt(input.val());
-		if (userInput === 10) {
-	 		banner.text(userInput +" is " + right);
-		} else {
-		banner.html(wrong);
-		}
+	banner.empty()
+	qfield.text(q_and_a[id].question); 
+
+	$("#submit").on('click', function() {
+
+	var userInput = parseInt(input.val());
+
+	if (userInput === q_and_a[id].answer ) {
+
+
+			
+	
+		return banner.text(userInput + " is " + right);
+
+	} else {
+		return banner.text(wrong);
+	}
 });
-       
-       });
+	show.on("click", function(){
+		return banner.text(q_and_a [id].hint);
+	})
 
-
-
-$("#2").on("click", function(){
- 		banner.empty();
-        qfiled.html(questions.b);
-        submit.on("click", function(){
-		var userInput = parseInt(input.val());
-		if (userInput === 8) {
-	 		banner.html(userInput +" is " + right);
-		} else {
-		banner.html(wrong);
-		}
 });
-       
-       });
 
-
-$("#3").on("click", function(){
- 		// qfiled.html(questions.a);
- 		banner.empty();
-        qfiled.html(questions.c);
-        submit.on("click", function(){
-		var userInput = parseInt(input.val());
-		if (userInput === 1) {
-	 		banner.html(userInput +" is " + right);
-		} else {
-		banner.html(wrong);
-		}
-});
-       
-       });
-
-$("#4").on("click", function(){
- 		// qfiled.html(questions.a);
- 		banner.empty();
-        qfiled.html(questions.d);
-        show.on("click", function () {
-			return banner.html("1 x 2 x 3 = 6 and 1 + 2 + 3 = 6 ");
-		})
-        submit.on("click", function(){
-		var userInput = (input.val());
-		if (userInput === "1 2 3") {
-	 		banner.html(userInput +" is " + right);
-		} else {
-		banner.html(wrong);
-		}
-		
-});
-       
-       });
-
-$("#5").on("click", function(){
- 		banner.empty();
-        qfiled.html(questions.f);
-        show.on("click", function () {
-			return banner.html("7 cartons for larges boxes per carton, where 7 x 8 = 56 boxes. 4 for small boxes, where 4 x 10 = 40 boxes. 96 total boxes and 11 cartons");
-		})
-        submit.on("click", function(){
-		var userInput = parseInt(input.val());
-		if (userInput === 11) {
-	 		banner.html(userInput +" is " + right);
-		} else {
-		banner.html(wrong);
-		}
-		
-});
-       
-       });
-
-
-       
-$("#6").on("click", function(){
- 		// location.reload();
- 		banner.empty();
-        qfiled.html(questions.g);
-        show.on("click", function () {
-				return banner.html("Once! Because after that it's not 100 anymore");
-			});
-        submit.on("click", function(){
-		var userInput = parseInt(input.val());
-		if (userInput === 1) {
-	 		banner.html(userInput +" is " + right);
-		} else {
-		banner.html(wrong);
-		}
-
-		
-});
-       
-       });
-
-$("#7").on("click", function(){
- 		// location.reload();
- 		banner.empty();
-        qfiled.html(questions.h);
-        show.on("click", function () {
-				return banner.html("3 people. one mother is the mother of another daughter. (2 mothers) another mother's child and her child. (2 daughters). 1grandma the oldest one there. 1 grandaughter the smallest.");
-			});
-        submit.on("click", function(){
-		var userInput = parseInt(input.val());
-		if (userInput === 3) {
-	 		return banner.html(userInput +" is " + right);
-		} else {
-		return banner.html(wrong);
-		}
-
-		
-});
-       
-       });
-
-
-
-
-
-
-
-
-
+// var updateScore = function () {
+// 	scrore.push("1");
+// }
 
 
 
