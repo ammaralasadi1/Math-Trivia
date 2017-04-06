87646445c0930bb31585679a182d70e606320603
Ammar Alasadi <alasadi.ammar@gmail.com>
2017-04-05 22:03:19 -0400

    Updated CSS and added sound, name input

--- cards.js
+++ cards.js
@@ -21,10 +21,19 @@ var questionArray= [
 ,{question: "A merchant can place 8 large boxes or 10 small boxes into a carton for shipping. In one shipment, he sent a total of 96 boxes. If there are more large boxes than small boxes, how many cartons did he ship?", answer: 11, hint:"7 cartons for larges boxes per carton, where 7 x 8 = 56 boxes. 4 for small boxes, where 4 x 10 = 40 boxes. 96 total boxes and 11 cartons"}
 ,{question:"How many times can you take away 2 from 100?", answer : 1, hint: "Once! Because after that it's not 100 anymore"}
 ,{question:"There are 2 mothers, 2 daughters, 1 grandma and 1 grandaughter. How many people are there?", answer: 3, hint:"3 people. one mother is the mother of another daughter. (2 mothers) another mother's child and her child. (2 daughters). 1grandma the oldest one there. 1 grandaughter the smallest."}
+,{question: "If a traffic light is red 48% of the time, and green 47% of the time, for how many hours and minutes would the amber light be illuminated in one day? ", answer: 1.2, hint: "5% of 24 hours = 1.2 hours = 1 hour and 12 minutes."}
+,{question: "A farmer had 15 sheep, and all but 8 died. How many are left?", answer: 8, hint: "8 of course."}
+,{question: "How many four cent postal cards are there in a dozen?", answer: 12, hint: "Dozen is 12"}
 ]
 var questionIndex =-1; 
 var currentAnswer; 
 var currentHint; 
+var userName = prompt("Who is the player", "your name goes here");
+if (userName === null || userName === undefined ) {
+	userName = ("")
+}
+
+
 
 
 
@@ -39,9 +48,10 @@ function update(id) { //A function to update the score
 		questionsAnswered.push(id); 
 		score++; 
 		localStorage.setItem('score',score);
-		localStorage.setItem('questionsAnswered', questionsAnswered); 
+		localStorage.setItem('questionsAnswered', questionsAnswered);
+		localStorage.setItem('userName', userName);
 	}
-	scoreDisplay.text("Your score: " + score + " out of 7");
+	scoreDisplay.text("Hey, "+userName+" your score is " + score + "/10");
 };
 
 $('#next').click(function() {
@@ -60,18 +70,22 @@ function onSubmit() {
 	var userInput = parseInt(input.val());
     if (userInput === currentAnswer ) {
 		banner.text(userInput + " is correct!");
+		var bell = new Audio('ff-bell.wav');
+		bell.play();
 		banner.css("color", "#00FF00");
 		update(questionIndex);
 		input.val("");
 
+
 		} 
 	else {
-			return banner.text("Wrong Answer!");
+			banner.text("Wrong Answer!");
+			var buzzer = new Audio('BUZZER.mp3');
+			buzzer.play();
 			input.val("");
+
 		}
 }
-
-
 
 $("#submit").on('click', onSubmit); 
 	
@@ -90,6 +104,7 @@ $("#reset").on("click", function(){
 	questionsAnswered = [];
 	localStorage.removeItem("score");
 	localStorage.removeItem("questionsAnswered");
+	localStorage.removeItem("userName");
 	scoreDisplay.text("Score is set to " + score);
 	// banner.hide();
 	// qfield.text("");
@@ -124,6 +139,7 @@ $(document).ready(function() {		//Hides Buttons and Show start.
 	input.hide();
 	start.fadeIn();
 	qfield.text("Hit Next to skip to the questions")
+	// userName();
 });
 
 
