d99f4955d698825718b44d85261f3923b471690e
Ammar Alasadi <alasadi.ammar@gmail.com>
2017-04-06 09:39:33 -0400

    HTML, CSS is validated / JS file cleaned

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# ammaralasadi1
+
+Math Trivia user story: 
+1. User is presented with a start option to launch the app. 
+2. The user is presented with a question.
+3. The user is able to answer the question by input.
+4. The user will be informed if the answer is "Correct" or "incorrect"
+5. The user score will be displayed while playing.
+6. The user is able to get the correct answer on demand. 
+7. The user score is stored even after a hard refresh. 
+8. The user is able to reset the score counter and the game.
+8. The user will get sound feedback if answered correctly and a different sound feedback if answered otherwise.
--- cards.js
+++ cards.js
@@ -13,29 +13,21 @@ if (score) score = parseInt(score); else score =0;
 var questionsAnswered = localStorage.questionsAnswered;
 if (questionsAnswered) questionsAnswered = questionsAnswered.split(','); else questionsAnswered = []; 
 
-var questionArray= [
+var questionArray = [
 {question: "8 + 2 = ?", answer: 10, hint: "10"}
 ,{question: "5 + 3 = ?", answer: 8, hint: "8"}
 ,{question: "6 - 6 + 6 / 6 = ?", answer: 1, hint: "1"}
-,{question: "Give me 3 positive numbers that will result the same when multiplied together or added together", answer: 123, hint: "1 + 2 + 3 = 6 and 1 * 2 * 3 = 6"}
+,{question: "Give me 3 positive numbers that will result the same when multiplied together or added together", answer: 123, hint: "1 + 2 + 3 = 6 || 1 * 2 * 3 = 6"}
 ,{question: "A merchant can place 8 large boxes or 10 small boxes into a carton for shipping. In one shipment, he sent a total of 96 boxes. If there are more large boxes than small boxes, how many cartons did he ship?", answer: 11, hint:"7 cartons for larges boxes per carton, where 7 x 8 = 56 boxes. 4 for small boxes, where 4 x 10 = 40 boxes. 96 total boxes and 11 cartons"}
 ,{question:"How many times can you take away 2 from 100?", answer : 1, hint: "Once! Because after that it's not 100 anymore"}
 ,{question:"There are 2 mothers, 2 daughters, 1 grandma and 1 grandaughter. How many people are there?", answer: 3, hint:"3 people. one mother is the mother of another daughter. (2 mothers) another mother's child and her child. (2 daughters). 1grandma the oldest one there. 1 grandaughter the smallest."}
-,{question: "If a traffic light is red 48% of the time, and green 47% of the time, for how many hours and minutes would the amber light be illuminated in one day? ", answer: 1.2, hint: "5% of 24 hours = 1.2 hours = 1 hour and 12 minutes."}
+,{question: "If a traffic light is red 48% of the time, and green 47% of the time, for how many hours and minutes would the amber light be illuminated in one day? ", answer:1.12, hint: "5% of 24 hours = 1.2 hours = 1 hour and 12 minutes."}
 ,{question: "A farmer had 15 sheep, and all but 8 died. How many are left?", answer: 8, hint: "8 of course."}
 ,{question: "How many four cent postal cards are there in a dozen?", answer: 12, hint: "Dozen is 12"}
 ]
 var questionIndex =-1; 
 var currentAnswer; 
 var currentHint; 
-var userName = prompt("Who is the player", "your name goes here");
-if (userName === null || userName === undefined ) {
-	userName = ("")
-}
-
-
-
-
 
 input.keypress(function(event) { // 
     if (event.which == 13) {
@@ -43,44 +35,40 @@ input.keypress(function(event) { //
         onSubmit(); 
     }
 });
+
 function update(id) { //A function to update the score
 	if (!questionsAnswered.includes(id)) {
 		questionsAnswered.push(id); 
 		score++; 
 		localStorage.setItem('score',score);
 		localStorage.setItem('questionsAnswered', questionsAnswered);
-		localStorage.setItem('userName', userName);
+		
 	}
-	scoreDisplay.text("Hey, "+userName+" your score is " + score + "/10");
+	scoreDisplay.text("Hey, your score is " + score + "/10");
 };
-
 $('#next').click(function() {
-	qfield.addClass("red");
 	banner.empty();
 	questionIndex++; 
 	var currentQuestion = questionArray[questionIndex];
 	qfield.text(currentQuestion.question); 
 	currentAnswer = currentQuestion.answer;
 	currentHint = currentQuestion.hint; 
-	
-
 }); 
-
 function onSubmit() {
 	var userInput = parseInt(input.val());
     if (userInput === currentAnswer ) {
 		banner.text(userInput + " is correct!");
-		var bell = new Audio('ff-bell.wav');
+		var bell = new Audio('sound/ff-bell.wav');
 		bell.play();
 		banner.css("color", "#00FF00");
 		update(questionIndex);
 		input.val("");
 
-
-		} 
+	} 
 	else {
 			banner.text("Wrong Answer!");
-			var buzzer = new Audio('BUZZER.mp3');
+			banner.css("color", "#d10e0e");
+			var buzzer = new Audio('sound/BUZZER.mp3');
 			buzzer.play();
 			input.val("");
 
@@ -88,8 +76,7 @@ function onSubmit() {
 }
 
 $("#submit").on('click', onSubmit); 
-	
-start.on("click", function () { // Shows Question buttons
+		start.on("click", function () { // Shows Question buttons
 		$(".score").fadeIn(1000);
 		questionButton.fadeIn();
 		show.fadeIn();
@@ -104,31 +91,7 @@ $("#reset").on("click", function(){
 	questionsAnswered = [];
 	localStorage.removeItem("score");
 	localStorage.removeItem("questionsAnswered");
-	localStorage.removeItem("userName");
 	scoreDisplay.text("Score is set to " + score);
-	// banner.hide();
-	// qfield.text("");
-	$(document).ready(function() {		//Hides Buttons and Show start.
-	$(".score").hide();
-	questionButton.hide();
-	reset.hide();
-	show.hide();
-	submit.hide();
-	input.hide();
-	start.fadeIn();
-	qfield.text("Hit Next to skip to the questions")
-});
-});
-					// Show hint to the question. 
-show.on("click", function(){ 
-	// qfield.addClass("blue");
-	return qfield.text("Answer: " + currentHint);
-	
-
-
-
-})
-
 
 $(document).ready(function() {		//Hides Buttons and Show start.
 	$(".score").hide();
@@ -139,7 +102,24 @@ $(document).ready(function() {		//Hides Buttons and Show start.
 	input.hide();
 	start.fadeIn();
 	qfield.text("Hit Next to skip to the questions")
-	// userName();
+	});
+});
+					// Show hint to the question. 
+show.on("click", function(){ 
+	return qfield.text("Answer: " + currentHint);
+	
+})
+
+$(document).ready(function() {		//Hides Buttons and Show start.
+	$(".score").hide();
+	questionButton.hide();
+	reset.hide();
+	show.hide();
+	submit.hide();
+	input.hide();
+	start.fadeIn();
+	qfield.text("Hit Next to skip to the questions")
+	
 });
 
 
